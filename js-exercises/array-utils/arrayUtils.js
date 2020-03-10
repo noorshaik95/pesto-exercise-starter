b81c3b796ab488d9bdd75c907c3979846d3fef5a
NoorUllah Shaik <noor.shaik95@hotmail.com>
2020-03-10 23:40:54 +0530

    Solve arrayUtils problem

--- js-exercises/array-utils/arrayUtils.js
+++ js-exercises/array-utils/arrayUtils.js
@@ -1,3 +1,33 @@
+function forEach(array, callback) {
+  for (let element of array) {
+    callback(element);
+  }
+}
+
+function map(array, callback) {
+  const mappedArray = [];
+  for (let element of array) {
+    mappedArray.push(callback(element));
+  }
+  return mappedArray;
+}
+
+function filter(array, callback) {
+  const filteredArray = [];
+  for (let element of array) {
+    if(callback(element)) {
+      filteredArray.push(element);
+    }
+  }
+  return filteredArray;
+}
+
+function reduce(array, callback, initialValue) {
+  for (let element of array) {
+    initialValue = callback(initialValue, element);
+  }
+  return initialValue;
+}
 
 export {
   forEach,
--- js-exercises/array-utils/arrayUtils.test.js
+++ js-exercises/array-utils/arrayUtils.test.js
@@ -1,1 +1,38 @@
 // Write your own test cases.
+import { forEach, filter, map, reduce } from './arrayUtils';
+
+describe('array methods', () => {
+  let testArray = [1, 2];  
+  it('forEach', () => {
+    const mockCallback = jest.fn(() => {}) ;
+    forEach(testArray, mockCallback)
+
+    expect(mockCallback.mock.calls.length).toBe(2);
+    expect(mockCallback.mock.calls[0][0]).toBe(1);    
+    expect(mockCallback.mock.calls[1][0]).toBe(2);    
+  });
+
+  it('map - with mockCallback', () => {
+    const mockCallback = jest.fn((element) => {}) ;    
+    map(testArray,mockCallback);
+    expect(mockCallback.mock.calls.length).toBe(2);
+    expect(mockCallback.mock.calls[0][0]).toBe(1);    
+    expect(mockCallback.mock.calls[1][0]).toBe(2);     
+  });
+
+  it('map - without mockCallback', () => {
+    const resultArray = map(testArray, (element) => element + 1);
+    expect(resultArray).toEqual([2,3]);   
+  });
+
+  test('filter', () => {
+    const resultArray = filter(testArray,(element) => element - 1 === 0);
+    expect(resultArray).toEqual([1]);
+  });
+
+  test('reduce', () => {
+    const reducedValue = reduce(testArray,(initialValue, element) => initialValue + element, 0)
+    expect(reducedValue).toBe(3);
+  });
+
+});
