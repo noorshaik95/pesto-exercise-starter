b57e1e5ca9c18ae2dc74470b11f3dacec483096e
NoorUllah Shaik <noor.shaik95@hotmail.com>
2020-03-11 16:12:15 +0530

    Solve deepCopyObject

--- js-exercises/deep-copy-object/deepCopyObject.js
+++ js-exercises/deep-copy-object/deepCopyObject.js
@@ -1,3 +1,19 @@
-const deepCopyObject = objToCopy => {};
+const deepCopyObject = objToCopy => {
+  if(typeof objToCopy !== "object" || objToCopy === null) {
+    return objToCopy;
+  } else if(Array.isArray(objToCopy)){
+    let replicaArray = [];
+    for(let element of objToCopy) {
+      replicaArray.push(deepCopyObject(element));
+    }
+    return replicaArray;
+  } else {
+    let replicaObject = {};
+    for(let key in objToCopy) {
+      replicaObject[key] = deepCopyObject(objToCopy[key]);
+    }
+    return replicaObject;
+  }
+};
 
 export { deepCopyObject };
