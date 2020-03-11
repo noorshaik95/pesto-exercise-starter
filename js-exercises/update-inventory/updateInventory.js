8c34dee2f79481a3bec578c46352467af4f66cf1
NoorUllah Shaik <noor.shaik95@hotmail.com>
2020-03-11 16:32:56 +0530

    Solve updateInventory

--- js-exercises/update-inventory/updateInventory.js
+++ js-exercises/update-inventory/updateInventory.js
@@ -1,5 +1,30 @@
-function updateInventory(...args) {
-  return args;
+function updateInventory(oldInventory, newInventory) {
+  const inventoryObject = {};
+  const sortedInventoryObject = {};
+  const updatedInventory = [];
+  
+  convertToObject(oldInventory, inventoryObject);
+  convertToObject(newInventory, inventoryObject);
+
+  Object.keys(inventoryObject).sort().map((key) => {
+    sortedInventoryObject[key] = inventoryObject[key];
+  });
+  
+  for(let key in sortedInventoryObject) {
+    updatedInventory.push([sortedInventoryObject[key], key]);
+  }
+  
+  return updatedInventory;
+}
+
+function convertToObject(inventory, inventoryObject) {
+  for(let item of inventory) {
+    if(!inventoryObject[item[1]]) {
+      inventoryObject[item[1]] = item[0];
+    } else {
+      inventoryObject[item[1]] += item[0];
+    }
+  }
 }
 
 export {
