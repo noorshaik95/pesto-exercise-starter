dd697b01db7c080145a114ee8ef6d8c17f2b7dc4
NoorUllah Shaik <noor.shaik95@hotmail.com>
2020-03-16 00:44:50 +0530

    Solve user-check-password problem

---
+++ js-exercises/permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -0,0 +1,11 @@
+/**
+ * @param {*[]} permutationOptions
+ * @return {*[]}
+ */
+function permutateWithoutRepetitions(permutationOptions) {
+  if(permutationOptions.length === 1) {
+    return permutationOptions;
+  }
+}
+
+export { permutateWithoutRepetitions };
