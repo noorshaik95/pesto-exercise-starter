cb5c87275c82c7478a3e404a923597adcc474405
NoorUllah Shaik <noor.shaik95@hotmail.com>
2020-03-16 18:18:32 +0530

    Solve pathSatisfies problem

--- js-exercises/path-satisfies/pathSatisfies.js
+++ js-exercises/path-satisfies/pathSatisfies.js
@@ -1,6 +1,12 @@
 
 function pathSatisfies(...args) {
-  return args;
+  const conditionCheck = args[0];
+  let valueToBeChecked = {...args[2]};
+  const keys = args[1];
+  for(let key of keys) {
+    valueToBeChecked = valueToBeChecked[key];
+  }
+  return conditionCheck(valueToBeChecked);
 }
 
 export {
